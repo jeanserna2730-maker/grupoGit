00c6b8c3144d4468784aa1b3b3906e7d097eca6e
Dilan Analuisa <dilan.analuisa5190@utc.edu.ec>
2026-06-04 01:48:11 -0600

    Isaacdi pro player

--- src/novale/MAYOR.java
+++ src/novale/MAYOR.java
@@ -18,6 +18,9 @@ public class MAYOR {
     System.out.printf("Numero 1: "); int n1=sc.nextInt();
     System.out.printf("Numero 2: "); int n2=sc.nextInt();
     System.out.printf("Numero 3: "); int n3=sc.nextInt();
+    System.out.println("-".repeat(20));
+    if (n1>n2 && n1>n3){
+    System.out.printf("NUMERO %d.MAYOR",n1);
     
     }
     
