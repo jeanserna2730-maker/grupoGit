c1c5a9e36e5df75cd114b91960d5df9684f1269b
DavCo <jhonny.corrales9555@utc.edu.ec>
2026-06-04 00:57:52 -0500

    cambios

--- src/novale/TABLASMULTI.java
+++ src/novale/TABLASMULTI.java
@@ -12,11 +12,11 @@ public class TABLASMULTI {
  public static void main(String[] args) {
  int j=0,i=0;
  System.out.println("TABLAS DE MULTIPLICAR \n"+ "".repeat(20));
- for (i=1;i<=3;i++){
+ for (i=1;i<=12;i++){
  System.out.printf("TABLA DEL: "+i+"\t\t");}
  System.out.println();
  for (j=1;j<=12;j++){
- for (i=1;i<=3;i++){
+ for (i=1;i<=12;i++){
  System.out.printf(i+"x"+j+"="+(i*j)+"\t\t\t"); }
  System.out.println();
  }
