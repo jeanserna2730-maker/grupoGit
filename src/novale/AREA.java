6bb87e17d9a3a194d1f78886c3dce6a8c0f70b7a
DavCo <jhonny.corrales9555@utc.edu.ec>
2026-06-03 23:27:33 -0500

    cambios David 2

---
+++ src/novale/AREA.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package novale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class AREA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Scanner sc =new Scanner(System.in);
+ System.out.println("RADIO");
+ double radio = sc.nextDouble();
+ double area=Math.PI*radio*radio;
+ System.out.printf("El area es: %.2f",area);
+ 
+    }
+    
+}
