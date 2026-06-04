88f72b015054e9bf1fbd14a982f0ab4bca6453c2
OneRayli <jean.serna2730@utc.edu.ec>
2026-06-04 01:17:34 -0500

    Jairo-import Scanner

--- src/novale/CALCU.java
+++ src/novale/CALCU.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package novale;
-
+import java.util.Scanner;
 /**
  *
  * @author USUARIO
@@ -14,7 +14,12 @@ public class CALCU {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingresa el primer número: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Ingresa el segundo número: ");
+        double num2 = scanner.nextDouble();
     }
     
 }
