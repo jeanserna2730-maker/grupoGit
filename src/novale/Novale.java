4de5be8090693c350ab36cb24c53c2e0e6423427
DavCo <jhonny.corrales9555@utc.edu.ec>
2026-06-03 23:24:16 -0500

    cambios David

--- src/novale/Novale.java
+++ src/novale/Novale.java
@@ -16,6 +16,7 @@ public class Novale {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("prueba");
+        System.out.println("hola mundo");
     }
     
 }
